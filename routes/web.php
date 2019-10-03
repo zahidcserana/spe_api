be17f995dead45fb9f48df0366845d53dcd76d66
hmrubai <rubai.mobarak@gmail.com>
2019-10-03 18:59:30 +0600

    Return done

--- routes/web.php
+++ routes/web.php
@@ -89,6 +89,8 @@ $router->group(['prefix' => 'api'],
 
                 $router->post('purchase/save', ['uses' => 'OrderController@purchaseSave']);
                 $router->post('purchase/due/save', ['uses' => 'OrderController@purchaseDueSave']);
+                $router->post('purchase/item/details/update', ['uses' => 'OrderController@purchaseItemDetailsUpdate']);
+                $router->post('purchase/item/delete', ['uses' => 'OrderController@purchaseItemDetailsDelete']);
                 
                 $router->post('purchase/previous/details', ['uses' => 'OrderController@previousPurchaseDetails']);
                 //purchase/list
