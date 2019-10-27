43fd22e29314abd62eb295a0eaea9c1b4db0e01f
hmrubai <rubai.mobarak@gmail.com>
2019-10-27 19:45:25 +0600

    Purchase report done

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -749,42 +749,39 @@ class OrderController extends Controller
     public function masterPurchaseList(Request $request){
 
         $data = [];
-        $orders = Order::select('orders.id', 'orders.invoice', 'orders.purchase_date', 'orders.status', 'orders.discount', 'orders.total_amount', 'orders.total_advance_amount', 'orders.total_due_amount', 'medicine_companies.company_name')
+        $orders = Order::select('orders.id', 'orders.invoice', 'orders.purchase_date', 'orders.status', 'orders.discount', 'orders.total_amount', 'orders.total_payble_amount', 'orders.total_advance_amount', 'orders.total_due_amount', 'medicine_companies.company_name', 'users.name as created_by')
         ->where('orders.status', 'ACCEPTED')
         ->leftjoin('medicine_companies', 'medicine_companies.id', '=', 'orders.company_id')
+        ->leftjoin('users', 'users.id', '=', 'orders.created_by')
         ->get();
 
         foreach($orders as $order):
             $order_id = $order->id;
             $itemList = [];
 
-            $orderItems = OrderItem::select('medicines.brand_name', 'medicines.strength', 'order_items.receive_qty', 'order_items.batch_no', 'order_items.exp_date')
+            $orderItems = OrderItem::select('medicines.brand_name', 'medicines.strength', 'order_items.pieces_per_box', 'order_items.trade_price', 'order_items.unit_price', 'order_items.box_vat', 'order_items.mrp', 'order_items.quantity', 'order_items.batch_no', 'order_items.exp_date')
             ->where('order_items.order_id', $order_id)
-            ->where('order_items.is_received', 1)
             ->leftjoin('medicines', 'medicines.id', '=', 'order_items.medicine_id')
             ->get();
 
             foreach($orderItems as $item):
+                $trade_price = $item->trade_price;
+                $box_vat = $item->box_vat;
+                $tp_with_vat = $trade_price + $box_vat;
+
                 $item_name = $item->brand_name . ' ' .$item->strength;
-                $itemList[] = array('medicine' => $item_name , 'receive_qty' => $item->receive_qty, 'batch_no' => $item->batch_no, 'exp_date' => $item->exp_date);
+
+                $itemList[] = array('medicine' => $item_name , 'unit_price_with_vat' => $item->unit_price, 'tp_with_vat' => $tp_with_vat, 'quantity' => $item->quantity, 'batch_no' => $item->batch_no, 'exp_date' => $item->exp_date);
             endforeach;
 
-            $data[] = array('invoice' => $order->invoice, 'purchase_date' => $order->purchase_date, 'discount' => $order->discount, 'total_amount' => $order->total_amount, 'total_advance_amount' => $order->total_advance_amount, 'total_due_amount' => $order->total_due_amount, 'company_name' => $order->company_name, 'items' => $itemList);
+            $data[] = array('invoice' => $order->invoice, 'purchase_date' => $order->purchase_date, 'created_by' => $order->created_by, 'discount' => $order->discount, 'total_amount' => $order->total_amount, 'total_payble_amount' => $order->total_payble_amount, 'total_advance_amount' => $order->total_advance_amount, 'total_due_amount' => $order->total_due_amount, 'company_name' => $order->company_name, 'items' => $itemList);
         endforeach;
 
         return response()->json(array(
             'data' => $data,
             'status' => 'Successful',
-            'message' => 'Inventory Purchase items list'
+            'message' => 'Purchase list'
         ));
-
-
-        // return response()->json(array(
-        //     'total' => 'total',
-        //     'page_no' => '$pageNo',
-        //     'limit' => '$limit',
-        //     'data' => '$orders',
-        // ));
     }
 
     public function purchaseListFilter(Request $request){
